dc08e333a9c5cad3e4edb539783ec33842dbf67f
PerrySJ <perry.150@wright.edu>
2026-09-10 12:09:24 -0400

    Started LabProblem2

---
+++ LabProblem2/LabProblem2.py
@@ -0,0 +1,34 @@
+#Weights
+zbW = 10
+attendanceW = 5
+labW = 42
+quizW = 10
+midtermW = 13
+finalW = 20
+totalW = 100
+
+#Grades
+zbG = float(input("What is your grade for zyBooks: "))
+print(f"You have a {zbG}% for zyBooks, which is {zbW}% of your final grade.")
+attendanceG = float(input("What is your grade for attendance: "))
+print(f"You have a {attendanceG}% for attendance, which is {attendanceW}% of your final grade.")
+labG = float(input("What is your grade for lab: "))
+print(f"You have a {labG}% for lab, which is {labW}% of your final grade.")
+quizG = float(input("What is your grade for quizzes: "))
+print(f"You have a {quizG}% for quizzes, which is {quizW}% of your final grade.")
+midtermG = float(input("What is your grade for midterm: "))
+print(f"You have a {midtermG}% for midterm, which is {midtermW}% of your final grade.")
+
+curWeightSum = zbW + attendanceW + labW + quizW + midtermW
+curWeightedGradeSum = (zbG * zbW) + (attendanceG * attendanceW) + (labG * labW) + (quizG * quizW) + (midtermG * midtermW)
+
+avgGrade = (curWeightedGradeSum) /curWeightSum
+
+print(f"{avgGrade: .2f}")
+
+excludedWeight = finalW
+desiredGrade = 90
+
+neededPercent =((desiredGrade * 100) - curWeightedGradeSum) / excludedWeight
+print(f"{neededPercent: .2f}")
+
